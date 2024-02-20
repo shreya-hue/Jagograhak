31737bb34fc7b4e75aca053239a275fb23cbb3c2
shreya.k.das <shreya.k.das@KDC1-L-3301NM5.dir.svc.accenture.com>
2024-02-21 01:54:58 +0530

    2nd commit

---
+++ Jagograhak-Assesment/src/com/accenture/lkm/business/bean/DateRangeBean.java
@@ -0,0 +1,26 @@
+package com.accenture.lkm.business.bean;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+public class DateRangeBean {
+
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date fromDate;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date toDate;
+	public Date getFromDate() {
+		return fromDate;
+	}
+	public void setFromDate(Date fromDate) {
+		this.fromDate = fromDate;
+	}
+	public Date getToDate() {
+		return toDate;
+	}
+	public void setToDate(Date toDate) {
+		this.toDate = toDate;
+	}
+	
+}
